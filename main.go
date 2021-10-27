fc0c043db045cdc26666ae3a21c36fb22cf249b8
fralcow <spacehit94@gmail.com>
2021-10-28 02:25:17 +0300

    move models to a separate file

--- main.go
+++ main.go
@@ -223,32 +223,3 @@ func deleteUser(w http.ResponseWriter, r *http.Request) {
 
 	render.Status(r, http.StatusNoContent)
 }
-
-type UserResponse struct {
-	*User
-	Id uint `json:"id"`
-}
-
-func NewUserResponse(id uint) *UserResponse {
-	resp := &UserResponse{Id: id}
-	if user, _ := dbGetUser(id); user != nil {
-		resp.User = user
-	}
-
-	return resp
-}
-
-func (ur *UserResponse) Render(w http.ResponseWriter, r *http.Request) error {
-	if ur.User == nil {
-		return UserNotFound
-	}
-	return nil
-}
-
-func NewUsersResopnse(userList *UserList) []render.Renderer {
-	list := []render.Renderer{}
-	for k := range *userList {
-		list = append(list, NewUserResponse(k))
-	}
-	return list
-}
